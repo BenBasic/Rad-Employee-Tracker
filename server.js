ed4a2598c33ea11bf980915f706c010f4b726038
BenBasic <benjaminmbasic@gmail.com>
2022-06-20 22:27:01 -0400

    Added main menu prompts and a switch to run a function based on what answer is selected in the main menu, currently I dont have the functions written out but Im working on that now.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { connect } = require("http2");
 const inquirer = require("inquirer");
 // Requirement allows running databases within server
 const mysql = require('mysql2');
+const { createConnection } = require("net");
 // Requirement allows printing console data as a table
 require("console.table");
 // Setting up the port, will run in process environment port, if local it will be 30001
@@ -28,7 +29,83 @@ db.connect(function (err) {
 function mainMenu() {
     inquirer.prompt([
         {
-            
+            type: "list",
+            message: "What would you like to do?",
+            name: "menu",
+            choices: 
+            [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "View employees by department",
+                "View all managers",
+                "Add a department",
+                "Add a role",
+                "Update an employee role",
+                "Update employee managers",
+                "Delete department",
+                "Delete role",
+                "Delete employee",
+                "Exit Program"
+            ]
         }
     ])
+
+    .then(function(answer) {
+        switch (answer.menu) {
+            case "View all departments":
+                viewDeparments();
+                break;
+
+            case "View all roles":
+                viewRoles();
+                break;
+
+            case "View all employees":
+                viewEmployees();
+                break;
+
+            case "View employees by department":
+                viewEmployeesDepartment();
+                break;
+            
+            case "View all managers":
+                viewManagers();
+                break;
+
+            case "Add a department":
+                addDepartment();
+                break;
+
+            case "Add a role":
+                addRole();
+                break;
+
+            case "Update an employee role":
+                updateRole();
+                break;
+            
+            case "Update employee managers":
+                updateManagers();
+                break;
+
+            case "Delete department":
+                deleteDepartment();
+                break;
+
+            case "Delete role":
+                deleteRole();
+                break;
+            
+            case "Delete employee":
+                deleteEmployee();
+                break;
+
+            case "Exit Program":
+                db.end();
+                break;
+            
+                
+        }
+    });
 }
