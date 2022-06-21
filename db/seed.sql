1f4d4af09f2ef3e0a1978368823e96d228fab60c
BenBasic <benjaminmbasic@gmail.com>
2022-06-21 00:00:34 -0400

    Inserted values to populate the database with seed.sql, created the first 2 of many mainMenu functions based on user input

---
+++ db/seed.sql
@@ -0,0 +1,48 @@
+-- Using the company_db database
+USE company_db;
+
+-- Inserting department values
+INSERT INTO department (name)
+VALUES ("Front Desk");
+INSERT INTO department (name)
+VALUES ("Cleaning");
+INSERT INTO department (name)
+VALUES ("Repair");
+INSERT INTO department (name)
+VALUES ("Accounting");
+
+-- Inserting role values
+INSERT INTO role (title, salary, department_id)
+VALUES ("Receptionist", 40000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Housekeeper", 50000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Maintenance", 60000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Book Keeper", 55000, 4);
+
+-- Inserting employee values for Receptionists
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Alex", "Smith", 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Gabby", "Doe", 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Muir", 1, 2);
+
+-- Inserting employee values for Housekeepers
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kate", "Jones", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Zoki", "Bane", 2, 3);
+
+-- Inserting employee values for Maintenance
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Dave", "Barnes", 3, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Nick", "Seed", 3, null);
+
+-- Inserting employee values for Book Keepers
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Manuela", "Read", 4, 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Geralt", 4, null);
--- server.js
+++ server.js
@@ -105,7 +105,31 @@ function mainMenu() {
                 db.end();
                 break;
             
-                
         }
+    });
+}
+
+function viewDeparments() {
+    // Selecting the name property from the departments table 
+    db.query("SELECT name FROM department;",
+    function(err, result) {
+        if (err) throw err;
+
+        console.table(result);
+        mainMenu();
+    });
+}
+
+function viewRoles() {
+    /* Selecting the first name and last name properties from the employee table, the title and salary properties from the role table, the name property from the department table.
+    Then renaming employee table to manager table, while selecting records that have matching role values in Role and Employee tables, and department values in department table and role table.
+    Then returning all matching employee id values on tables with manger_id and id properties
+    */
+    db.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, AS Manager FROM employee INNER JOIN role ON role.id = employee.role_id INNER JOIN department ON department.id = role.department_id left join employee e ON employee.manager_id = e.id;",
+    function(err, result) {
+        if (err) throw err;
+
+        console.table(result);
+        mainMenu();
     });
 }
